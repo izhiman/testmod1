97df9885eb556e39c5c44bd90bdfe500e74a6e20
SmartMan <zhiman.mzm@antfin.com>
2022-08-21 01:23:51 +0800

    'first'

---
+++ HelloGo.go
@@ -0,0 +1,8 @@
+package  testmod1
+
+import "fmt"
+
+func SayHelloToSomeOne(someone string)  string{
+	return fmt.Sprintf("Hello %s", someone)
+}
+
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/izhiman/testmod1
